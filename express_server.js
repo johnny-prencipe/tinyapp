31d989e39b1cd1b4ea6533ca2a83bfe4967ac7d3
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-09 06:23:24 +0000

    added random string gen

--- express_server.js
+++ express_server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 }
 
+const generateRandomString = () => {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
+
+// post logic for when a form is submitted
+app.post('/urls', (req, res) => {
+  console.log(req.body); //log the POST request to the console
+  res.send('ok'); //respond with 'ok'
+});
+
 // URL tree
+app.get('/urls/new', (req, res) => res.render('urls_new'));
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
