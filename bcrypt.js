2da1133eff6b0d76214712096c52f42ffb20e81e
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-11 06:12:27 +0000

    removing a test file from git

--- bcrypt.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const plaintext = 'asd'
-
-bcrypt.hash(plaintext, saltRounds, function(err, hash) {
-  if (err) return err;
-  console.log(hash);
-});
-
